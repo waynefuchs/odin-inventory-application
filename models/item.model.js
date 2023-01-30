d119bac1157e9f621179bbabb4672d1a8b54e492
Wayne Fuchs <wayne.fuchs@icloud.com>
2023-01-30 15:10:45 -0500

    Implement database models and placeholder data

---
+++ models/item.model.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const ItemSchema = new Schema({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  category: { type: String, required: true },
+  price: { type: Number, required: true },
+  available: { type: Number, required: true },
+});
+
+ItemSchema.virtual("url").get(function () {
+  return `/catalog/item/${this._id}`;
+});
+
+module.exports = mongoose.model("Item", ItemSchema);
